950b9cda03b6744598efd65f62f84601c2769faa
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-08-26 01:00:22 +0600

    1 to 100 prime number and total prime numbe

--- helloa.c
+++ helloa.c
@@ -2,33 +2,29 @@
 //Prime number With loop
 #include<stdio.h>
 int main()
-
 {
-    int i,num,count=0;
-    scanf("%d",&num);
-    if(num<=1)
-       {
-        printf("Not a prime number\n");
-        return 0;    
-       }        //Because otherwise, the note 0,1 will be  printed 
-                           //not a prime numbernot a prime number p .
-        
-        
+    int i,num,count=0,totalprimenumbers=0;
 
-       
-   for(i=2;i<num;i++)
-       {
-         if(num%i==0)
+
+    for(num=2;num<=100;num++)
+    {
+        count=0;
+        for(i=2;i<sqrt(num);i++)
           {
-           count++;
-           break;
-          }
-       }   //for  er kaj ekhanei sesh
-   if(count==0)
-       {
-        printf("Prime a number\n");
-       }
-  else 
-      printf("Not a prime number\n");
+            if(num%i==0)
+           {
+            count++;
+            break;
+            }
+        }
+        if(count==0)
+        
+        {
+           printf(" %d ",num);
+          totalprimenumbers++;
+          
+        }
+    }
+    printf("\n %d",totalprimenumbers);
     return 0;
 }
