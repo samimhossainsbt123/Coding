9e560aad9ac99156565428e0dd5808570f875a87
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-08-26 01:17:25 +0600

    1

---
+++ samim.c
@@ -0,0 +1,30 @@
+
+//Prime number With loop
+#include<stdio.h>
+int main()
+{
+    int i,num,count=0,totalprimenumbers=0;
+
+
+    for(num=2;num<=100;num++)
+    {
+        count=0;
+        for(i=2;i<sqrt(num);i++)
+          {
+            if(num%i==0)
+           {
+            count++;
+            break;
+            }
+        }
+        if(count==0)
+        
+        {
+           printf(" %d ",num);
+          totalprimenumbers++;
+          
+        }
+    }
+    printf("\n %d",totalprimenumbers);
+    return 0;
+}
