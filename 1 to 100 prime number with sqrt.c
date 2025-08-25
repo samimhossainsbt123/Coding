0c8df3563725f39a6d7b4ee24af7595b1acbf898
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-08-25 23:36:00 +0600

    1 to 100 prime number with sqrt.c

--- 1 to 100 prime number with sqrt.c
+++ 1 to 100 prime number with sqrt.c
@@ -8,8 +8,8 @@ int main()
 
     for(num=2;num<=100;num++)
     {
-        count=0;
-        for(i=2;i<sqrt(num);i++)
+        count=0;  //must dibo
+        for(i=2;i<sqrt(num);i++)// pr for(i=2;i<num;i++)
           {
             if(num%i==0)
            {
