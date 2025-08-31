d915725bc62093fb132f505f65163f80e6ca6d56
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-01 01:20:57 +0600

    37.  (*Value Round).c

---
+++ 37.  (*Value Round).c
@@ -0,0 +1,12 @@
+//Round value
+#include<stdio.h>
+int main()
+{
+   float x,result;
+    scanf("%f",&x);
+    result=round(x);// 3.4=3,3.6=4---- mane 0.5 er niche hole oi sonkhay ta nahole porer sonkha
+   printf("%f\n",result);
+   return 0;
+}
+
+
