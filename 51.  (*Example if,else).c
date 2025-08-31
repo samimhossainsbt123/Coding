778bde6c64eeff11338b0b9ba0c4e32a33c4a56d
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-01 04:01:19 +0600

    51.  (*Example if,else).c

---
+++ 51.  (*Example if,else).c
@@ -0,0 +1,21 @@
+//Example if,else
+#include<stdio.h>
+int main()
+{
+   int num;
+   scanf("%d",&num);
+   if(num>=100)
+   {
+    printf("Hello! Bangladesh\n");
+    printf("Kamal Hossain Shamim\n");
+   }
+   else
+   {
+    printf("Ok ,Good bye\n");
+    printf("Shamim\n");
+   }
+   return 0;
+
+}
+
+
