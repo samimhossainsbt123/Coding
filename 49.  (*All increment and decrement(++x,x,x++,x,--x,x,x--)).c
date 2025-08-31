b1deec55c0d24e0c32cea04387bd1ce3d04c3c4c
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-01 03:47:34 +0600

    49.  (*All increment and decrement(++x,x,x++,x,--x,x,x--)).c

---
+++ 49.  (*All increment and decrement(++x,x,x++,x,--x,x,x--)).c
@@ -0,0 +1,18 @@
+//All increment and decrement(++x,x,x++,x,--x,x,x--)
+#include<stdio.h>
+int main()
+{
+   int x;
+   scanf("%d",&x);
+
+    printf("++X= %d\n",++x);
+        printf("X= %d\n",x);
+            printf("X++= %d\n",x++);
+                printf("X= %d\n",x);
+                    printf("--X= %d\n",--x);
+                        printf("X= %d\n",x);
+                            printf("X--= %d\n",x--);
+                                printf("X= %d\n",x);
+
+  
+   return 0;
