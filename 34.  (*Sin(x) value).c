3bcf9e57cc3ba408b7570ade6948b9239085eb10
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-01 01:15:21 +0600

    34.  (*Sin(x) value).c

---
+++ 34.  (*Sin(x) value).c
@@ -0,0 +1,12 @@
+//Sin(x) value
+#include<stdio.h>
+int main()
+{
+   float x,result;
+    scanf("%f",&x);
+    result=sin(x);// accurate value hoyna cal are code er
+   printf("%f\n",result);
+   return 0;
+}
+
+
