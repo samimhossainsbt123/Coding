def56832f1049c15a1f4ab47c259fa484f492eb4
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-01 00:37:35 +0600

    28. (*absulate value(positive all)any number).c

---
+++ 28. (*absulate value(positive all)any number).c
@@ -0,0 +1,12 @@
+// absulate value(positive all)any number
+#include<stdio.h>
+int main()
+{
+   int x,result;
+   scanf("%d",&x);
+   result=abs(x);//or result =abs(-x);
+   printf("%d\n",result);
+   return 0;
+}
+
+
