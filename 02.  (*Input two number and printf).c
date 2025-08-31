a656fae2b59c456ede4f41db4ab33e776f3ab258
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-08-31 12:45:52 +0600

    (*Input two number and printf).c

---
+++ 02.  (*Input two number and printf).c
@@ -0,0 +1,12 @@
+//Input two number and printf
+#include<stdio.h>
+int main()
+{ 
+  int num1,num2;
+  scanf("%d",&num1);
+    scanf("%d",&num2);
+printf("num1 = %d\n",num1);
+printf("num2 = %d\n",num2);
+  return 0;
+
+}
