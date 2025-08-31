7bf148693ea2aa137f272a3e0253d7a6a83e3309
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-01 03:33:39 +0600

    46.  (*Postfix increment(x++)).c

---
+++ 46.  (*Postfix increment(x++)).c
@@ -0,0 +1,13 @@
+//Postfix increment(x++)
+#include<stdio.h>
+int main()
+{
+   int x,y;
+   scanf("%d",&x);
+   y=x++;
+    printf("X= %d\n",x);//postfix e x boro,y choto
+   printf("Y= %d",y);
+   return 0;
+}
+
+
