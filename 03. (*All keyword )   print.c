fc1710cbf73416a202777dd320a3e633f396a8bc
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-08-31 17:35:06 +0600

    03. (*All keyword )   print.c

--- 03. (*All keyword )   print.c
+++ 03. (*All keyword )   print.c
@@ -6,7 +6,7 @@ int main()
    float num3=89.9;
    double num4=78.765;
    char ch='a';
-   printf("Numbers are num1& num2=%d  %d\n",num1,num2);
+   printf("Numbers are num1& num2=%d  %d\n",num1,num2);//
    printf("Num3 =%f\n",num3);
      printf("Num4=%lf\n",num4);;
        printf("Charecter=%c\n",ch);
