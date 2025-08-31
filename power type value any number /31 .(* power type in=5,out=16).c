9229b8acc22f195cd07294c73b09adea2e29673e
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-01 00:48:15 +0600

    31 .(* power type in=5,out=16).c

---
+++ power type value any number /31 .(* power type in=5,out=16).c
@@ -0,0 +1,15 @@
+// power type value any number
+// ex.input 5 output 16
+#include<stdio.h>
+int main()
+{
+   float x=2,y,z,result;
+ 
+     scanf("%f",&y);
+     z=y-1;
+ result=pow(x,z);
+   printf("%f\n",result);
+   return 0;
+}
+
+
