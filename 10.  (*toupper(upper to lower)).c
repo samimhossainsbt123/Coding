3de0662de5e79d41b7f3bc13406856bdb7bbc992
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-08-31 15:35:47 +0600

    (*toupper(upper to lower)).c

---
+++ 10.  (*toupper(upper to lower)).c
@@ -0,0 +1,12 @@
+// toupper(lower to upper) 
+#include<stdio.h>
+int main()
+{
+  char lower,upper;
+  scanf("%c",&lower);
+  upper=toupper(lower);
+  printf("upper=%c\n", upper);
+                                 
+  return 0;
+}
+
