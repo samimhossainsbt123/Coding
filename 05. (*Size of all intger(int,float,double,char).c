ddfe13b581900d2390894e461e3877b95353e94f
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-08-31 17:35:49 +0600

    05. (*Size of all intger(int,float,double,char).c

--- 05. (*Size of all intger(int,float,double,char).c
+++ 05. (*Size of all intger(int,float,double,char).c
@@ -7,7 +7,7 @@ int main()
    double d;
    char c;
    printf("Sixe of integer=%d\n",sizeof(i));
-   printf("Size of float=%d\n",sizeof(f));
+   printf("Size of float=%d\n",sizeof(f));//
      printf("Size of double=%d\n",sizeof(d));
        printf("Size of char=%d\n",sizeof(c));
        return 0;
