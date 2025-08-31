6387f6bb175383cf5046497132b1a32723535881
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-08-31 17:25:37 +0600

    22.  (*Three number sum and avg).c

---
+++ 22.  (*Three number sum and avg).c
@@ -0,0 +1,17 @@
+// Three number sum and avg
+#include<stdio.h>
+int main()
+{
+  float num1,num2,num3,sum,avg;
+
+
+  scanf("%f%f%f",&num1,&num2,&num3);
+  sum=num1+num2+num3;
+  avg=(float)sum/3;
+
+  printf("Average=%f\n",avg);
+   
+                                 
+  return 0;
+}
+
