da28e22130778f17f2f137b03e47d7343ff9ca40
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-08-31 12:22:21 +0600

    All keyword    print.c

---
+++ All keyword    print.c
@@ -0,0 +1,15 @@
+//All keyword  print
+#include<stdio.h>
+int main()
+{ 
+   int num1=30,num2=90;
+   float num3=89.9;
+   double num4=78.765;
+   char ch='a';
+   printf("Numbers are num1& num2=%d  %d\n",num1,num2);
+   printf("Num3 =%f\n",num3);
+     printf("Num4=%lf\n",num4);;
+       printf("Charecter=%c\n",ch);
+       return 0;
+
+}
