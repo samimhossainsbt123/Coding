3abfc58549ea32b9639493059e9a1b5f4f237efd
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-08-31 17:50:23 +0600

    24. (* Circle area).c

---
+++ 24. (* Circle area).c
@@ -0,0 +1,12 @@
+//  Circle area
+#include<stdio.h>
+int main()
+{
+ float radius,area;
+
+  scanf("%f",&radius);
+  area=3.1416*radius*radius;
+  printf("Area=%f\n",area);
+                                  
+  return 0;
+}
