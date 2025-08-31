ec468c8b77bf880cec8176ff04494956ffb18429
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-01 00:44:33 +0600

    30.  (*power x,y value any number).c

---
+++ 30.  (*power x,y value any number).c
@@ -0,0 +1,13 @@
+// power x,y value any number
+#include<stdio.h>
+int main()
+{
+   float x,y,result;
+   scanf("%f",&x);
+     scanf("%f",&y);
+ result=pow(x,y);
+   printf("%f\n",result);
+   return 0;
+}
+
+
