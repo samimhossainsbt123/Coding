41d648692e267d6dd9a771ee04d3a4f3e36a5532
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-08-31 12:13:55 +0600

    Size of all intger(int,float,double,char).c

---
+++ Size of all intger(int,float,double,char).c
@@ -0,0 +1,15 @@
+//Size of all intger(int,float,double,char)
+#include<stdio.h>
+int main()
+{ 
+   int i;
+   float f;
+   double d;
+   char c;
+   printf("Sixe of integer=%d\n",sizeof(i));
+   printf("Size of float=%d\n",sizeof(f));
+     printf("Size of double=%d\n",sizeof(d));
+       printf("Size of char=%d\n",sizeof(c));
+       return 0;
+
+}
