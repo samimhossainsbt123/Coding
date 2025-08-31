9f52a05e2b15e6d0a00ee879647b9b0625dcb2a7
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-01 03:36:51 +0600

    47.  (*Prefix decrement(--x)).c

---
+++ 47.  (*Prefix decrement(--x)).c
@@ -0,0 +1,13 @@
+//Prefix decrement(--x)
+#include<stdio.h>
+int main()
+{
+   int x,y;
+   scanf("%d",&x);
+   y=--x;
+    printf("X= %d\n",x);//prefix decrement a x,y duitai 1 kore kombe
+   printf("Y= %d",y);
+   return 0;
+}
+
+
