55134932fed23d667a0adff25afd9c8785fcedeb
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-01 01:16:27 +0600

    35.  (*Cos(x) value).c

---
+++ 35.  (*Cos(x) value).c
@@ -0,0 +1,11 @@
+//Cos(x) value
+#include<stdio.h>
+int main()
+{
+   float x,result;
+    scanf("%f",&x);
+    result=cos(x);// accurate value hoyna cal are code er
+   printf("%f\n",result);
+   return 0;
+}
+
