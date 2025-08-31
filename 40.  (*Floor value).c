6854197fdd0786f13ccf69cbce45618566170aaf
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-01 01:25:47 +0600

    40.  (*Floor value).c

---
+++ 40.  (*Floor value).c
@@ -0,0 +1,12 @@
+//Floor value
+#include<stdio.h>
+int main()
+{
+   float x,result;
+    scanf("%f",&x);
+    result=floor(x);//soman or tar theke choto
+   printf("%f\n",result);
+   return 0;
+}
+
+
