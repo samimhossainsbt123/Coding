36804e1320b1be902d8b3c3a3b5962298b76c08f
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-01 04:28:43 +0600

    54.  (*Positive ,Negative,Zero).c

---
+++ 54.  (*Positive ,Negative,Zero).c
@@ -0,0 +1,21 @@
+//Positive ,Negative,Zero
+#include<stdio.h>
+int main()
+{
+   char ch;
+   scanf("%c",&ch);
+   if(ch=='a'||ch=='a'||ch=='A'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='e'||ch=='E'||ch=='u'||ch=='U')
+   {
+    printf("Vowel\n");
+   }
+   
+   else
+   {
+    printf("Consonent\n");
+   }
+  
+ 
+   return 0;
+}
+
+
