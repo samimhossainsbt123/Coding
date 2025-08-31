2efaadda2c218170c9306af20744678c3c23b993
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-08-31 13:30:26 +0600

    (*Lower to upper).c

---
+++ 08.  (*Lower to upper).c
@@ -0,0 +1,10 @@
+//Lower to upper
+#include<stdio.h>
+int main()
+{
+  char lower,upper;
+  scanf("%c",&lower);
+  printf("Upper =%c\n",lower-32);// orupper=lower-32;
+                                  // then printf uppper
+  return 0;
+}
