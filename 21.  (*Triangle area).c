539365990b0a9e4cd91bb73c5c3cbc37742ad170
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-08-31 17:18:57 +0600

    21.  (*Triangle area).c

---
+++ 21.  (*Triangle area).c
@@ -0,0 +1,15 @@
+//  Triangle area
+#include<stdio.h>
+int main()
+{
+  float base,height,area;
+
+  scanf("%f%f",&base,&height);
+  area=0.5*(base*height);
+
+  printf("Area=%f\n",area);
+   
+                                 
+  return 0;
+}
+
