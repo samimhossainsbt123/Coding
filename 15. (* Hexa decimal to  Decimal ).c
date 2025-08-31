79f7ba308c3f175a6a06f736bc5e33c9dc7cb8e2
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-08-31 16:09:45 +0600

    15. (* Hexa decimal to  Decimal ).c

---
+++ 15. (* Hexa decimal to  Decimal ).c
@@ -0,0 +1,11 @@
+//   Hexa decimal to  Decimal 
+#include<stdio.h>
+int main()
+{
+  int num;
+  scanf("%x",&num);
+  printf("Decimal=%d\n",num);
+                                 
+  return 0;
+}
+
