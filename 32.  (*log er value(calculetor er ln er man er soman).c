8c6af22d15405ccbcc4b214ee49a662757b460b2
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-01 00:56:38 +0600

    32.  (*log er value(calculetor er ln er man er soman).c

---
+++ 32.  (*log er value(calculetor er ln er man er soman).c
@@ -0,0 +1,13 @@
+//log er value(calculetor er ln er man er soman)
+#include<stdio.h>
+int main()
+{
+   float x,result;
+    scanf("%f",&x);
+    result=log(x);//log er man calculator er ln er maner soman
+                  // are log10 er man calculator er log er maner soman  
+   printf("%f\n",result);
+   return 0;
+}
+
+
