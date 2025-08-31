41c0ac7648520bd5163ba246f817fd3675e0c824
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-01 03:55:48 +0600

    50.  (*Zero,Even ,Odd).c

---
+++ 50.  (*/50.  (*Zero,Even ,Odd).c
@@ -0,0 +1,23 @@
+//Zero,Even ,Odd
+#include<stdio.h>
+int main()
+{
+   int num;
+   scanf("%d",&num);
+   if(num==0)
+   {
+    printf("Zero\n");
+   }
+   else if(num%2==0)
+   {
+    printf("Even\n");
+   }
+   else 
+   {
+    printf("Odd\n");
+   }
+ 
+   return 0;
+}
+
+
