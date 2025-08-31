c235a58e3390bb945042c662580b99ca7ed87fc4
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-08-31 16:29:50 +0600

    19.  (* Sum and average).c

---
+++ 19.  (* Sum and average).c
@@ -0,0 +1,13 @@
+//  Sum and average
+#include<stdio.h>
+int main()
+{
+  int num1,num2;
+  float avg,sum;
+  scanf("%d%d",&num1,&num2);
+  sum= num1+num2;
+  avg =sum/2;
+  printf("Avg=%f\n",avg);
+                                 
+  return 0;
+}
