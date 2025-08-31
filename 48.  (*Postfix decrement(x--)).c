28df0262e6d56d004750f4749455f2657a8aea10
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-01 03:39:16 +0600

    48.  (*Postfix decrement(x--)).c

---
+++ 48.  (*Postfix decrement(x--)).c
@@ -0,0 +1,13 @@
+//Postfix decrement(x--)
+#include<stdio.h>
+int main()
+{
+   int x,y;
+   scanf("%d",&x);
+   y=x--;
+    printf("X= %d\n",x);//postfix decrement a x choto y boro x kombe
+   printf("Y= %d",y);
+   return 0;
+}
+
+
