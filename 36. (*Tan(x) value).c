adbe27da6f9c34e6a286347dcfaa671e87811b0f
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-01 01:18:11 +0600

    36. (*Tan(x) value).c

---
+++ 36. (*Tan(x) value).c
@@ -0,0 +1,12 @@
+//Tan(x) value
+#include<stdio.h>
+int main()
+{
+   float x,result;
+    scanf("%f",&x);
+    result=tan(x);// accurate value hoyna cal are code er
+   printf("%f\n",result);
+   return 0;
+}
+
+
