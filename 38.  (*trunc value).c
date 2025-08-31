a27203b22e3df2f6986eec24d261e85a5e9d6bdc
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-01 01:23:06 +0600

    38.  (*trunc value).c

---
+++ 38.  (*trunc value).c
@@ -0,0 +1,12 @@
+//trunc value
+#include<stdio.h>
+int main()
+{
+   float x,result;
+    scanf("%f",&x);
+    result=trunc(x);// 3.4=3,3.6=3---- mane dosmik er porer sonkhar kono dam nai shudu purno shonkha print hobe
+   printf("%f\n",result);
+   return 0;
+}
+
+
