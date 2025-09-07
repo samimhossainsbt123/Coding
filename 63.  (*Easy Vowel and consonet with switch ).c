ea51b93feb9ff03ebb12a7778584def654a5262b
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-08 01:15:55 +0600

    63.  (*Easy Vowel and consonet with switch ).c

---
+++ 63.  (*Easy Vowel and consonet with switch ).c
@@ -0,0 +1,34 @@
+//  Easy Vowel and consonet with switch 
+#include<stdio.h>
+int main()
+{
+     char ch;
+     scanf("%c",&ch);
+      
+     switch(ch)//switch a---switch,case,break.default;
+     {
+          case 'a':
+          case 'A':
+
+          case 'u':
+
+          case 'U':
+
+          case 'e':
+          
+          case 'E':
+       
+          case 'i':
+        
+          case 'I':
+         
+          case 'o':
+         
+          case 'O':
+         printf("Vowel\n");
+          break;
+          default:
+          printf("Consonent\n");
+     }
+     return 0;
+}
