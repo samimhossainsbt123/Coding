139de9fe738864797e4c3db574ab53a93fffbf68
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-09 00:39:41 +0600

    75. (*Reverse of any digit).c

---
+++ 75. (*Reverse of any digit).c
@@ -0,0 +1,16 @@
+// Reverse of any digit
+#include<stdio.h>
+int main()
+{
+    int sum=0,num,temp,rem;
+    scanf("%d",&num);
+    temp=num;
+    while(temp!=0)
+    {
+     rem=temp%10;
+     sum=sum*10+rem;
+     temp=temp/10;
+    }
+   printf("reverse of digit=%d\n",sum);
+    return 0;
+}
