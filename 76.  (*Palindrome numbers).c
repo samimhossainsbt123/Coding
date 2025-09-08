cfc455f35f0bfbc73bcd813cc7a49ebe094fc42f
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-09 00:51:24 +0600

    76.  (*Palindrome numbers).c

---
+++ 76.  (*Palindrome numbers).c
@@ -0,0 +1,21 @@
+// Palindrome numbers
+#include<stdio.h>
+int main()
+{
+    int sum=0,num,temp,rem;
+    scanf("%d",&num);
+    temp=num;
+    while(temp!=0)
+    {
+     rem=temp%10;
+     sum=sum*10+rem;
+     temp=temp/10;
+    }
+   if(num==sum)
+   {
+     printf("Palindrome numbers\n");
+   }
+   else 
+   printf("Not prime numbers\n");
+    return 0;
+}
