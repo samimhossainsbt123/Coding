97f79788198febd0395920d208f2e5c64501d05c
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-10 00:31:28 +0600

    102. (* Patter  type 02 ultA(.....1234,123,12,1) same  bhabe@,#,*,row,ABC.AB.A).c

---
+++ 102. (* Patter  type 02 ultA(.....1234,123,12,1) same  bhabe@,#,*,row,ABC.AB.A).c
@@ -0,0 +1,16 @@
+// Patter  type 02 ultA(.....1234,123,12,1) same  bhabe@,#,*,row,ABC.AB.A
+#include<stdio.h>
+int main()
+{
+    int row,col,num;
+    scanf("%d",&num);
+    for(row=num-1;row>=1;row--)
+    {
+        for(col=1;col<=row;col++)
+        {
+            printf("%d ",col);
+        }
+        printf("\n");//for row er je braccet asse tar modde rakbo
+    }
+    return 0;
+}
