1095c99940a7b1affb9613ebe28dec99f002d133
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-09 11:47:14 +0600

    86. (*1*2+2*3+3*4+....+num1*num2).c

---
+++ 86. (*1*2+2*3+3*4+....+num1*num2).c
@@ -0,0 +1,17 @@
+// 1*2+2*3+3*4+....+num1*num2
+
+#include<stdio.h>
+int main()
+{
+   int i=1,j=2,num1,num2,sum=0;
+   scanf("%d",&num1);
+      scanf("%d",&num2);
+   while(i<=num1 && j<=num2)
+   {
+    sum=sum+(i*j);
+    i++;
+    j++;
+   }
+    printf("Sum=%d\n",sum);
+    return 0; 
+}
