fe552d7c45408c5ff9cb53daca45707a69c5a0f3
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-09 23:39:24 +0600

    93.  (*Fibonacci Series).c

--- 93.  (*Fibonacci Series).c
+++ 93.  (*Fibonacci Series).c
@@ -1,10 +1,10 @@
-// Fibonacci Seriouse
+// Fibonacci Series
 #include<stdio.h>
 int main()
 {
-    int num,count=0,fibo,first=0,second=1;
+    int num,count=0,fibo,first=0,second=1;//prime ,fibo,lucas holei count thakbei 
     scanf("%d",&num);
-    while(count<num)
+    while(count<num)// eta fibo and locus series modde hoy
     {
         if(count<=1)
         {
