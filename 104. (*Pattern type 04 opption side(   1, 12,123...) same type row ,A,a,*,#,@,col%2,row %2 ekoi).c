adf1eca0a658cce525f8690df4a0e4da8f6a7431
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-10 01:15:35 +0600

    104. (*Pattern type 04 opption side(   1, 12,123...) same type row ,A,a,*,#,@,col%2,row %2 ekoi).c

---
+++ 104. (*Pattern type 04 opption side(   1, 12,123...) same type row ,A,a,*,#,@,col%2,row %2 ekoi).c
@@ -0,0 +1,22 @@
+// Pattern type 04 opption side(   1, 12,123...) same type row ,A,a,*,#,@,col%2,row %2 ekoi
+#include<stdio.h>
+int main()
+{
+    int row,col,num;
+    scanf("%d",&num);
+    for(row=1;row<=num;row++)
+    {
+        for(col=1;col<=num-row;col++)  // space er jonno eta fixed
+        {
+            printf(" ");
+        }
+       {
+         for(col=1;col<=row;col++)
+        {
+            printf("%d",col);
+        }
+       }
+        printf("\n");//for row er je braccet asse tar modde rakbo
+    }
+    return 0;
+}
