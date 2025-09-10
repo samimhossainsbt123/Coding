c7caa03a7ea311c74ce667824a636cd74e223a5f
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-10 13:46:47 +0600

    122.  (* Goto statement( with label)).c

---
+++ 122.  (* Goto statement( with label)).c
@@ -0,0 +1,15 @@
+// Goto statement( with label)
+#include<stdio.h>
+int main()
+{ 
+     int i=1; // initialization
+       printf: // Lebel is call
+         printf("%d\n",i);
+
+         i++;   //update
+         if(i<20)   // condition
+        goto printf;  // label is calling here
+    return 0;
+}
+
+     
