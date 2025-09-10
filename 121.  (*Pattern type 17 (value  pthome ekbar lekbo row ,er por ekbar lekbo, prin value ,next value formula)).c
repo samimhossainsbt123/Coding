1a45e71e29f0e4c6aba141a2a7b9800bf5ea0e19
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-10 13:42:43 +0600

    121.  (*Pattern type 17 (value  pthome ekbar lekbo row ,er por ekbar lekbo, prin value ,next value formula)).c

---
+++ 121.  (*Pattern type 17 (value  pthome ekbar lekbo row ,er por ekbar lekbo, prin value ,next value formula)).c
@@ -0,0 +1,25 @@
+// Pattern type 17 (value  pthome ekbar lekbo row ,er por ekbar lekbo, prin value ,next value formula)
+#include<stdio.h>
+int main()
+{
+    int row,col,num,value=1;
+    scanf("%d",&num);
+    for(row=0;row<num;row++)
+    {     value=1;   // mone rakbo row er pore 
+        for(col=1;col<=num-row;col++)
+        {
+            printf(" ");
+        }
+        for(col=0;col<=row;col++)
+        {
+               printf("%d  ",value);      
+            value= value * (row-col) / (col+1);
+            
+ 
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+     
