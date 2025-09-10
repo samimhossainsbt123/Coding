47e2deea5bff0bb1cf8d8e50c20928ee303f331f
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-09-10 11:28:42 +0600

    118.  (* Pattern  type 14(1,121,12321,1234321,...)).c

---
+++ 118.  (* Pattern  type 14(1,121,12321,1234321,...)).c
@@ -0,0 +1,26 @@
+// Pattern  type 14(1,121,12321,1234321,...)
+#include<stdio.h>
+int main()
+{
+    int row,col,num;
+    scanf("%d",&num);
+    for(row=1;row<=num;row++)
+    {
+        for(col=1;col<=num-row;col++)// space er jonno eta fixed
+        {
+            printf("  ");
+        }
+       
+         for(col=1;col<=row;col++)
+        {
+            printf("%d ",col);
+        }
+      for(col=row-1;col>=1;col--)
+      {
+           printf("%d ",col);
+      }
+    
+        printf("\n");//for row er je braccet asse tar modde rakbo
+    }
+    return 0;
+}
