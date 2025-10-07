b40bcdb84720b21e15e98c861721e247b527407a
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-10-08 02:29:30 +0600

    125.(*Array with sum real).c

---
+++ 125.(*Array with sum real).c
@@ -0,0 +1,15 @@
+// Array with sum real
+
+#include <stdio.h>
+int main()
+{ 
+ int num[]={10,20,40,50,57,90},sum,i;
+for(i=0;i<6;i++)
+{
+    sum=sum+num[i];
+}
+printf("%d  ",sum);
+
+return 0;
+
+}
