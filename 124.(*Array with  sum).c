26dce84807cbe43c0d918977b3fc7242f8264074
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-10-08 02:25:37 +0600

    124.(*Array with  sum).c

---
+++ 124.(*Array with  sum).c
@@ -0,0 +1,11 @@
+// Array with sum only sum
+#include <stdio.h>
+int main()
+{ 
+ int num[]={10,20,40,50,57,90},sum;
+ sum=num[0]+num[1]+num[2]+num[3]+num[4]+num[5];
+printf("%d  ",sum);
+
+return 0;
+
+}
