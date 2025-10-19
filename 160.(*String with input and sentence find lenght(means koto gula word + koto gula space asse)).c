06c0ad9376f909c3f982be3b11746e85e2ba7d66
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-10-19 21:31:51 +0600

    160.(*String with input and sentence find lenght(means koto gula word + koto gula space asse)).c

---
+++ 160.(*String with input and sentence find lenght(means koto gula word + koto gula space asse)).c
@@ -0,0 +1,10 @@
+// String with input and sentence find lenght(means koto gula word + koto gula space asse)
+#include<stdio.h>
+int main()
+{
+    char str[20];
+   gets(str);
+   int len=strlen(str);// string er str formula diya shorte kora hoy,,,,ta na hole i+while diya sob korte hoy
+   printf("%d",len);  // %d karon ekhane sonkha print korbo tai
+    return 0;
+}   
