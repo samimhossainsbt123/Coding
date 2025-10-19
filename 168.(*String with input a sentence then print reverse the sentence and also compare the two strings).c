4f789f9742374a675bee1a66a0f2cbf70c70c274
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-10-20 05:50:14 +0600

    168.(*String with input a sentence then print reverse the sentence and also compare the two strings).c

---
+++ 168.(*String with input a sentence then print reverse the sentence and also compare the two strings).c
@@ -0,0 +1,37 @@
+// String with input a sentence then print reverse the sentence and also compare the two strings
+
+#include<stdio.h>
+int main()
+{
+    char str1[1000];
+    char str2[1000];
+    gets(str1);
+    int i=0,len=0,j; // j er man 0 nibona 
+    while(str1[i]!=0)
+    {
+      len++;
+      i++;
+    }
+  for(j=0,i=len-1;i>=0;i--,j++) /// bhinno niom mukosto
+  {
+    str2[j]=str1[i];
+  }
+  str2[j]='\0';
+    printf("%s\n",str2);
+
+
+    int d=strcmp(str1,str2);
+    if(d==0)
+    {
+      printf("Strings are equal");
+    }
+    else
+    {
+      printf("Strings are not equal");
+    }
+     
+ 
+
+    return 0;
+      
+}
