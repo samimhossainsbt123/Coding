b349ae5632f7e4a11465442bf7adc3a3574af08e
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-10-19 21:06:02 +0600

    158.(*String with multiple word or  sentence input and print).c

---
+++ 158.(*String with multiple word or  sentence input and print).c
@@ -0,0 +1,9 @@
+// String with multiple word or  sentence input and print
+#include<stdio.h>
+int main()
+{
+    char str[20];
+   gets(str);   //multiple word or sentence er jonno
+    printf("%s",str);
+    return 0;
+}   
