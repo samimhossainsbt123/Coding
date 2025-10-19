165467e4bf015d09ed3c4d99ab54f772f404b212
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-10-19 21:01:25 +0600

    156.(*String with a word print).c

---
+++ 156.(*String with a word print).c
@@ -0,0 +1,8 @@
+// String with a word print
+#include<stdio.h>
+int main()
+{
+    char str[]={'K','A','M','A','L','\0'};  // Null character obbossoi dite hobe
+    printf("%s",str);
+    return 0;
+}   
