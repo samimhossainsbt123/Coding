bebe87a7649e1aae0dcfc9b54b5b165d1224686b
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-10-19 21:04:16 +0600

    157.(*String with input a word and print).c

---
+++ 157.(*String with input a word and print).c
@@ -0,0 +1,9 @@
+// String with input a word and print
+#include<stdio.h>
+int main()
+{
+    char str[20];
+    scanf("%s",&str); // eta shudu ekta word er jonno ekadik hole gets(str); hoy
+    printf("%s",str);
+    return 0;
+}   
