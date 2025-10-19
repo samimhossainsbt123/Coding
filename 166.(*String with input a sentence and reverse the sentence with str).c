bbf4c087e99973a85fdc01032d2f9cf082192b61
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-10-20 05:38:13 +0600

    Add program to reverse a given sentence166.(*String with input a sentence and reverse the sentence with str).c

---
+++ 166.(*String with input a sentence and reverse the sentence with str).c
@@ -0,0 +1,14 @@
+// String with input a sentence and reverse the sentence with str
+#include<stdio.h>
+int main()
+{
+    char str1[1000];
+    gets(str1);
+    strrev(str1); \\ main formula
+    printf("%s",str1);
+     
+ 
+
+    return 0;
+      
+}
