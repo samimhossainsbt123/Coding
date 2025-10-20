ae957a3d83334e5e4429082ace20695743e5aafb
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-10-20 15:15:38 +0600

    171.(*String with a sentence print all letter capital letter ).c

---
+++ 171.(*String with a sentence print all letter capital letter ).c
@@ -0,0 +1,13 @@
+// String with a sentence print all letter capital letter 
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    gets(str);
+    strupr(str); // formula
+    
+    printf("%s",str);
+    
+    return 0;
+      
+}
