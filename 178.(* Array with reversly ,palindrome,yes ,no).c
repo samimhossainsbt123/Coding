5599a89f3ebb6ffd05860fc572f2e2a8a6951d37
samimhossainsbt123 <samimhossainsbt@gmail.com>
2025-10-27 03:23:40 +0600

    178.(* Array with reversly ,palindrome,yes ,no).c

--- 178.(* Array with reversly ,palindrome,yes ,no).c
+++ 178.(* Array with reversly ,palindrome,yes ,no).c
@@ -2,24 +2,24 @@
 #include<stdio.h>
 int main()
 {
-    int N,i,k;
+    int N,i,j;
     scanf("%d",&N);
     int A[N];
     for(i=0;i<N;i++)
     {
         scanf("%d",&A[i]);
     }
-    for(k=N-1;k>=0;k--)  // ulta dhek theke asbo tobe n-1 karon 0 theke suru n-1 er ag porjonto scan nissi ,,tai n-1 theke suru korbo
+    for(j=N-1;j>=0;j--)  // ulta dhek theke asbo tobe n-1 karon 0 theke suru n-1 er ag porjonto scan nissi ,,tai n-1 theke suru korbo
     {
-        printf("%d ",A[k]);
+        printf("%d ",A[j]);
     }
-    if(A[i]!=A[k])  // jodi scanf and printf soman hoy
+    if(A[i]==A[j])  // jodi scanf and printf soman hoy
     {
-        printf("\nNO\n");
+        printf("\n Yes ,palindrome\n");
     }
     else
     {
-        printf("\nYES\n");
+        printf("\nNot palindrome\n");
     }
      return 0;
       
